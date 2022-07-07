89c23e7632b9b133781a78e5fa4757442dfa30a2
Adrien Tirlemont <Adrien.Tirlemont@insight.com>
2022-07-07 17:33:12 +0200

    Ajout fc AddTask

---
+++ BlazorAppTodoList/Data/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppTodoList.Data.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
